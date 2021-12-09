ecbfc25e80e215f7ed110d02967f6cfcd7b05603
p22strautz <p22strautz@fairfieldprep.org>
2021-12-09 10:32:58 -0500

    finish 8-1

--- hw8-1-1.py
+++ hw8-1-1.py
@@ -1,2 +1,17 @@
 # Author: SCT (AMDG) 12/9/21
 
+def totalpoints(freethrow, twopoint, threepoint):
+    totalpoints = 0
+    totalpoints = (1 * freethrow) + (2 * twopoint) + (3 * threepoint)
+    return totalpoints
+
+
+
+print(totalpoints(3,2,1))
+
+print(totalpoints(1,2,3))
+
+print(totalpoints(1,1,1))
+
+
+
